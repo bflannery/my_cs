ed89a46e585352c3310bf0cf3e5f8d1957371c5c
Brian <bflannery66@gmail.com>
2018-07-18 17:21:29 -0500

    sequences

--- 61A/Chp2/ex.py
+++ 61A/Chp2/ex.py
@@ -1,6 +1,7 @@
 from fractions import gcd
+from operator import add
 
-
+# Treat rationals as whole data values
 def add_rationals(x, y):
     nx, dx = numer(x), denom(x)
     ny, dy = numer(y), denom(y)
@@ -18,7 +19,7 @@ def print_rational(x):
 def rationals_are_equal(x, y):
     return numer(x) * denom(y) == numer(y) * denom(x)
 
-
+# Treat rationals as numerators and denominators
 def rational(n, d):
     g = gcd(n, d)
     return n // g, d // g
@@ -46,3 +47,225 @@ def select(p, i):
     """Return the element at index i of pair p."""
     return p(i)
 
+
+def divisors(n):
+    return [1] + [x for x in range(2, n) if n % x == 0]
+
+
+def width(area, height):
+    assert area % height == 0
+    return area // height
+
+
+def perimeter(width, height):
+    return 2 * width + 2 * height
+
+
+def minimum_perimeter(area):
+    heights = divisors(area)
+    perimeters = [perimeter(width(area, h), h) for h in heights]
+    return min(perimeters)
+
+
+def apply_to_all(map_fn, s):
+    return [map_fn(x) for x in s]
+
+
+def keep_if(filter_fn, s):
+    return [x for x in s if filter_fn(x)]
+
+
+def reduce(reduce_fn, s, initial):
+    reduced = initial
+    for x in s:
+        reduced = reduce_fn(reduced, x)
+    return reduced
+
+
+def divisors_of(n):
+    divides_n = lambda x: n % x == 0
+    return [1] + keep_if(divides_n, range(2, n))
+
+
+def sum_of_divisors(n):
+    return reduce(add, divisors_of(n), 0)
+
+
+def perfect(n):
+    return sum_of_divisors(n) == n
+
+# TREES
+
+
+def tree(root_label, branches=[]):
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [root_label] + list(branches)
+
+
+def label(tree):
+    return tree[0]
+
+
+def branches(tree):
+    return tree[1:]
+
+
+def is_tree(tree):
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+
+def is_leaf(tree):
+    return not branches(tree)
+
+
+def fib_tree(n):
+    if n == 0 or n == 1:
+        return tree(n)
+    else:
+        left, right = fib_tree(n-2), fib_tree(n-1)
+        fib_n = label(left) + label(right)
+        return tree(fib_n, [left, right])
+
+
+def count_leaves(tree):
+    if is_leaf(tree):
+        return 1
+    else:
+        branch_counts = [count_leaves(b) for b in branches(tree)]
+        return sum(branch_counts)
+
+
+def partition_tree(n, m):
+    """Return a partition tree of n using parts of up to m."""
+    if n == 0:
+        return tree(True)
+    elif n < 0 or m == 0:
+        return tree(False)
+    else:
+        left = partition_tree(n-m, m)
+        right = partition_tree(n, m-1)
+        return tree(m, [left, right])
+
+
+def print_parts(tree, partition=[]):
+    if is_leaf(tree):
+        if label(tree):
+            print( ' + '.join(partition))
+    else:
+        left, right = branches(tree)
+        m = str(label(tree))
+        print_parts(left, partition + [m])
+        print_parts(right, partition)
+
+
+def right_binarize(tree):
+    """Construct a right-branching binary tree."""
+    if is_leaf(tree):
+        return tree
+    if len(tree) > 2:
+        tree = [tree[0], tree[1:]]
+    return [right_binarize(b) for b in tree]
+
+
+empty = 'empty'
+
+
+def is_link(s):
+    """s is a linked list if it is empty or a (first, rest) pair."""
+    return s == empty or (len(s) == 2 and is_link(s[1]))
+
+
+def link(first, rest):
+    """Construct a linked list from its first element and the rest."""
+    assert is_link(rest), "rest must be a linked list."
+    return [first, rest]
+
+
+def first(s):
+    """return the first element of a linked list s."""
+    assert is_link(s), "first only applies to linked lists."
+    assert s != empty, "empty linked list has no first element."
+    return s[0]
+
+
+def rest(s):
+    """Return the rest of the elements of a linked list s."""
+    assert is_link(s), "rest only applies to linked lists."
+    assert s != empty, "empty linked list has no rest."
+    return s[1]
+
+
+def len_link(s):
+    """Return the length of linked list s."""
+    length = 0
+    while s != empty:
+        s, length = rest(s), length + 1
+    return length
+
+
+def len_link_recursive(s):
+    """Return the length of linked list s."""
+    if s == empty:
+        return 0
+    return 1 + len_link_recursive(rest(s))
+
+
+def getitem_link(s, i):
+    """Return the element at index i of linked list s."""
+    while i > 0:
+        s, i = rest(s), i - 1
+    return first(s)
+
+
+def getitem_link_recurisve(s, i):
+    """Return the element at index i of linked list s."""
+    if i == 0:
+        return first(s)
+    return getitem_link_recurisve(rest(s), i - 1)
+
+
+def extend_link(s, t):
+    """Return a list with the elements of s followed by those of t."""
+    assert is_link(s) and is_link(t)
+    if s == empty:
+        return t
+    else:
+        return link(first(s), extend_link(rest(s), t))
+
+
+def apply_to_all_link(f, s):
+    """Apply f to each element of s."""
+    assert is_link(s)
+    if s == empty:
+        return s
+    else:
+        return link(f(first(s)), apply_to_all_link(f, rest(s)))
+
+
+def keep_if_link(f, s):
+    """Return a list with elements of s for which f(e) is true."""
+    assert is_link(s)
+    if s == empty:
+        return s
+    else:
+        kept = keep_if_link(f, rest(s))
+        if f(first(s)):
+            return link(first(s), kept)
+        else:
+            return kept
+
+
+def join_link(s, separator):
+    """Return a string of all elements in s separated by separator."""
+    if s == empty:
+        return ""
+    elif rest(s) == empty:
+        return str(first(s))
+    else:
+        return str(first(s)) + separator + join_link(rest(s), separator)
